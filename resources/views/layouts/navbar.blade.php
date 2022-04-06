79ce86f7a071b35b6348eaa421aafa9feb0fa221
Bewar Hizirvan <bewar@hizirvan.email>
2022-04-06 09:02:39 +0300

    updated

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,10 +3,16 @@
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
         <div class="container-fluid py-1 px-3">
             <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-
                 <ul class="navbar-nav  justify-content-end">
                     <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
                         <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                            <span class="sidenav-toggler-line text-white">Menu</span>
+                        </a>
+
+                    </li>
+                    <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+
+                        <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav1">
                             <div class="sidenav-toggler-inner">
                                 <i class="sidenav-toggler-line bg-white"></i>
                                 <i class="sidenav-toggler-line bg-white"></i>
@@ -18,5 +24,6 @@
             </div>
         </div>
     </nav>
+
     <!-- End Navbar -->
 @endsection
